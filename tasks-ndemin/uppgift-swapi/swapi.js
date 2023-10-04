5f07e191824f8fe89b2ee03613edd8cf68736966
doctorbond <michael.rostlund@hotmail.com>
2023-10-04 21:43:22 +0200

    asdasdasd

--- tasks-ndemin/uppgift-swapi/swapi.js
+++ tasks-ndemin/uppgift-swapi/swapi.js
@@ -7,33 +7,36 @@ document.getElementById('fetchCharacter').addEventListener('click', () => {
 });
 
 async function fetchAPI(api) {
-  const fetchResults = await fetch(api);
-  const data = await fetchResults.json();
-
- console.log(data);
 
  
- let {count, next, previous, results} = data;
-
- const planetList = data.results;
- let planetInfo = '';
-
- planetList.forEach((value, index) => {
+    const fetchResults = await fetch(api);
+    const data = await fetchResults.json();
   
-  const {name, rotation_period, orbital_period, diameter, climate} = value;
+    console.log(data);
+  
+   
+   let {count, next, previous, results} = data;
+  
+   const planetList = data.results;
+   let planetInfo = '';
+  
+   planetList.forEach((value, index) => {
+    
+    const {name, rotation_period, orbital_period, diameter, climate} = value;
+  
+    const planetPage = document.createElement('div');
+    planetInfo += `
+    <h2>${name}</h2>
+    <h4>Climate:${climate}</h4>
+    <h4>Diameter:${diameter}</h4>
+    `;
+    planetPage.className = 'planet-page';
+    console.log(value);
+   });
+  
+   displayElement.innerHTML = planetInfo;
+  
 
-  const planetPage = document.createElement('div');
-  planetInfo += `
-  <h2>${name}</h2>
-  <h4>Climate:${climate}</h4>
-  <h4>Diameter:${diameter}</h4>
-  `;
-
-  planetPage.className = 'planet-page';
-  console.log(value);
- });
-
- displayElement.innerHTML = planetInfo;
 }
 
 
